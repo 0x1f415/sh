52713748184a6a28d40af7ae84233f4c414044b9
0x1f415 <0x1f415@users.noreply.github.com>
2016-03-30 21:35:13 -0400

    don't duplicate self

--- stats.sh
+++ stats.sh
@@ -26,14 +26,16 @@ fi
 export sedexp="s/.*load averages\?: [0-9]\+\.[0-9]\+,\? \([0-9]\+\.[0-9]\+\).*/\1/g"
 
 info(){
-	UPTIME=$(timeout 2 ssh $1 "uptime" 2>/dev/null || return 1) || (echo -e "$RED$1: down$DEFAULT" && return 1) || return 1
+	if [[ $1 == $(hostname | sed -e "s/.local$//g") ]]
+	then
+		UPTIME=$(uptime | sed "$sedexp")
+	else
+		UPTIME=$(timeout 2 ssh $1 "uptime" 2>/dev/null || return 1) || (echo -e "$RED$1: down$DEFAULT" && return 1) || return 1
+	fi
 
 	echo -en "$LIGHT_BLUE$1$DEFAULT: "
 	echo $UPTIME | sed "$sedexp"
 }
-
-echo -en "$LIGHT_BLUE$(hostname)$DEFAULT: "
-uptime | sed "$sedexp"
 
 export -f info
 
